f73578263d106efc8580efc979d0708ba713e8f2
Cleber Varçal <cleber.varcal@varcalsys.com.br>
2016-09-06 05:12:20 -0300

    Criada camada Infra.Data com Uow

---
+++ DDD_Sample/src/Domain/Contracts/Repositories/Base/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Base;
+
+namespace Domain.Contracts.Repositories.Base
+{
+    public interface IRepositoryBase<T> where T: EntityBase
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DDD_Sample/src/Infra.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.UnitOfWork
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        void BeginTran();
+        void Save();
+        void SaveAsync();
+        void Commit();
+        void Rollback();
+    }
+}
