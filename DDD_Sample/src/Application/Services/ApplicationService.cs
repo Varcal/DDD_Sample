3a9b74537c31d9cbb396a5ec57270ebdf6f7d9fd
Cleber Varçal <cleber.varcal@varcalsys.com.br>
2016-09-06 06:45:24 -0300

    Criada a camada Application

---
+++ DDD_Sample/src/Application/Services/ApplicationService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infra.Data.UnitOfWork;
+using SharedKernel.DomainEvents;
+using SharedKernel.DomainEvents.Events.DomainNotifications;
+using SharedKernel.DomainEvents.Handlers.Base;
+
+namespace Application.Services
+{
+    public abstract class ApplicationService
+    {
+        private readonly Handler<DomainNotification> _notifications;
+        private readonly IUnitOfWork _uow;
+
+        protected ApplicationService(IUnitOfWork uow)
+        {
+            _notifications = DomainEvent.Container.GetService<Handler<DomainNotification>>();
+            _uow = uow;
+        }
+
+        public void BeginTran()
+        {
+            _uow.BeginTran();
+        }
+
+        public void Commit()
+        {
+            _uow.Commit();
+        }
+
+        public void RollBack()
+        {
+            _uow.Rollback();
+        }
+
+        public bool Save()
+        {
+            if (_notifications.HasNotification())
+                return false;
+
+            _uow.Save();
+            return true;
+        }
+    }
+}
